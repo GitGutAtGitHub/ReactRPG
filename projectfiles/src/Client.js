993500ac7974a11286e4f555e770d139bcea24b8
GitGutAtGitHub <58419128+GitGutAtGitHub@users.noreply.github.com>
2021-04-06 13:31:58 +0200

    login setup start

---
+++ projectfiles/src/Client.js
@@ -0,0 +1,48 @@
+import React, {useEffect, useState} from 'react';
+
+
+function LogIn() {
+    return(
+            <>
+                <h1>Log In</h1>
+                <label>Username:</label>
+                <br/>
+                <input name="username" value={name} onChange={onChange}></input>
+                <br/>
+                <br/>
+                <label>Password:</label>
+                <br/>
+                <input id='passwordInput'></input>
+                <br/>
+                <br/>
+                <button>Log in</button>
+            </>
+        );
+}
+
+function OnLoginBtnClick() {
+    let loginData = {username, password};
+    loginData.username = usernameInput.value;
+}
+
+function useDate() {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        let id = setInterval(() => {
+            updateTime();
+        }, 1000);
+
+        return () => {
+            clearInterval(id);
+        };
+    });
+
+    function updateTime() {
+        setTime(new Date);
+    }
+
+    return time;
+}
+
+export default LogIn;
